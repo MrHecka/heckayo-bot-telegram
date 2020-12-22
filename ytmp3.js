af4b3af9f7550b1659879f0d52cdf821b5507e00
Muhammad Mamad <muhammadmamad41@gmail.com>
2020-12-22 13:50:11 +0700

    fixed ytmp3

--- ytmp3.js
+++ ytmp3.js
@@ -8,49 +8,25 @@ const bot = new TeleBot({
     token: process.env.TOKEN
 })
 
-let nama = await Math.floor(Math.random() * Math.floor(1000))
-
 module.exports = bot => {
 
-function sendFile(msg, file){
-    msg.reply.file(file)
-}
-
 bot.on(/^\/ytmp3 (.+)$/, async (msg, props) => {
-  try {
-    const text = props.match[1];
-    if(!ytdl.validateURL(text)){
-        msg.reply.text('Link youtube tidak valid!');
+    const url = props.match[1];
+    if(ytdl.validateURL(url)){
+      let aac_file = ytdl.getURLVideoID(url) + ".mp3";
+      msg.reply.text("Sedang mendownload...sabar ngab...");
+      ytdl(url, {quality: "highestaudio", filter: "audioonly"})
+        .pipe(fs.createWriteStream(aac_file).on('finish',()=>{
+          msg.reply.text("Sedang mengirim...");
+          msg.reply.audio(aac_file).then(()=>{
+            fs.unlinkSync(aac_file);
+            msg.reply.text("Berhasil😎👌")
+          });
+        }));
+    }else{
+      msg.reply.text("Video tidak ditemukan...");
     }
-    ytdl.getInfo(text, {filter:"audioonly"}, (err, info) => {
-        if (err) throw err;
-        msg.reply.text(info);
-
-        // detect kalau file nya udah ada
-        if (fs.existsSync(info.title+nama+'.mp3')) {
-            sendFile(msg, info.title+nama+'.mp3');
-            return;
-        }
-
-        msg.reply.text('Starting Download...');
-        ytdl(text,{filter:"audioonly"})
-        .pipe(fs.createWriteStream(info.title+nama+'.mp3'))
-        .on('finish', () => {
-            sendFile(msg, info.title+nama+'.mp3');
-            fs.unlinkSync(info.title+nama+'.mp3', (error) => console.log(error));
-        });
-        
-        //hapus setelah 24 jam
-        setTimeout(fs.unlinkSync, 24*60*60*1000, info.title+'.mp3');
-      });
-} catch (e) {
-    msg.reply.text('Error ngab '+ e);
+  })
 }
-  });
-}
-
-
-
-
 
 
