6ebd0618bede7a4b9f32850c69c869524278621a
Muhammad Mamad <muhammadmamad41@gmail.com>
2020-12-24 16:29:38 +0700

    fix again

--- twtdl.js
+++ twtdl.js
@@ -28,9 +28,10 @@ await client.get(`statuses/show/${urlregex}`, async function(error, tweets, resp
   if (!error) {
     let nama = await tweets.user.screen_name
     let deskripsi = await tweets.text
-    let media = await tweets.extended_entities.media[0].video_info.variants[0].url
-    if(media === undefined) {
+    if(tweets.extended_entities.media[0].video_info.variants[0].url === undefined) {
         return bot.sendMessage(msg.from.id, `Error | Video tidak ditemukan! ${error}`)
+    } else if(tweets.extended_entities.media[0].video_info.variants[0].url != undefined) {
+    let media = await tweets.extended_entities.media[0].video_info.variants[0].url
     }
     await bot.sendMessage(msg.from.id, `ID Twitter Terdeteksi => ${urlregex}`)
     await bot.sendMessage(msg.from.id, `😎Berhasil Mendapatkan Data Tweet👌\n\nUsername : ${nama}\n\nDeskripsi : ${deskripsi}`)
