de51a0663517ba0437bfd001d6b0acfdc99e9f9e
Muhammad Mamad <muhammadmamad41@gmail.com>
2020-12-28 01:38:08 +0700

    up

--- nulis3.js
+++ nulis3.js
@@ -12,9 +12,9 @@ const bot = new TeleBot({
 
 module.exports = bot => {
     bot.on(/^\/nulis3 ([\s\S]+)/, async (msg, args) => {
-    let tulisan = await args.match[1]
+    let tulisan = await args.match[1].trim()
     await bot.sendMessage(msg.from.id, `Sabar ngab lagi nulis....`)
-    const splitText = await tulisan.replace(/(\S+\s*){1,10}/g, '$&\n')
+    const splitText = await tulisan.replace(/(\S+\s*){1,8}/g, '$&\n')
     const fixHeight = await splitText.split('\n').slice(0, 31).join('\n')
     await spawn('convert', [
         './nulis/sebelum.jpg',
@@ -23,11 +23,11 @@ module.exports = bot => {
         '-size',
         '960x1280',
         '-pointsize',
-        '22',
+        '27',
         '-interline-spacing',
-        '2',
+        '-6',
         '-annotate',
-        '+140+153',
+        '+140+148',
         fixHeight,
         `./nulis/setelah${msg.from.id}.jpg`
     ])
